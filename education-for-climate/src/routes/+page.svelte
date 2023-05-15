8818d4af7b096e84a2762e61f269a2ab0faa81ce
Héloïse Feldmann <feldmann.heloise@gmail.com>
2023-05-15 10:50:12 +0200

    start svelte

--- education-for-climate/src/routes/+page.svelte
+++ education-for-climate/src/routes/+page.svelte
@@ -1,2 +1,40 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import * as d3 from 'd3';
+    import { onMount } from 'svelte';
+	let user = { loggedIn: false };
+
+	function toggle() {
+		user.loggedIn = !user.loggedIn;
+	}
+
+    let cats = [
+        { id: 1, name: 'chipchat', place: 'ozo'},
+        { id: 2, name: 'Poupouss', place: 'rosoux'},
+        { id: 3, name: 'Darla', place: 'Liège'},
+
+    ];
+</script>
+
+<style>
+button {
+        padding: 10px;
+        border-radius: 5px;
+        color:black;
+        background-color: aquamarine;
+        border-color: aquamarine;
+    }
+</style>
+
+{#if user.loggedIn}
+	<button on:click={toggle}>
+		Log out
+	</button>
+{:else}
+	<button on:click={toggle}>
+		Log in
+	</button>
+{/if}
+
+{#each cats as cat }
+<p> {cat.name}</p>
+{/each}
