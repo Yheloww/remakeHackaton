78d60e673ca4507579941f55a9757bf9fe92bd92
Héloïse Feldmann <feldmann.heloise@gmail.com>
2023-05-16 15:37:02 +0200

    understanding scrolly

--- education-for-climate/src/routes/+page.svelte
+++ education-for-climate/src/routes/+page.svelte
@@ -1,81 +1,30 @@
 <script>
-    import data from "../data/data";
-    import * as d3 from 'd3'
-    import Scrolly from "../components/Scrolly.svelte";
+  import Scroller from "../components/scroller.svelte";
 
-    let width = 1000;
-    let height = 500;
-
-    $: xScale = d3.scaleLinear()
-    .domain([0,100])
-    .range([0,width/5]);
-
-    let currentStep;
-    const steps = ["<p>Step 0!</p>", 
-				   "<p>Step 1?</p>", 
-				   "<p>Step 2.</p>",
-                    ""];
-
-</script>
-
-<section>
-    <div class="container" bind:clientWidth={width}>
-        <svg {height} {width}>
-            {#each data as circle}
-            <circle 
-            cx={width/2} 
-            cy=250 r={xScale(circle.pourcentage)} 
-            fill="none" 
-            stroke={ circle.pourcentage == 100 ? "green" : "black"}
-            stroke-width="2px"/>
-            {/each}
-        </svg>
-    </div>
-	<Scrolly bind:value={currentStep}>
-		{#each steps as text, i}
-			<div class="step" class:active={currentStep === i}>
-				<div class="step-content">
-					{@html text}
-				</div>
-			</div>
-		{/each}
-	</Scrolly>
-</section>
-
-
-<style>
-    .container {
-        background: whitesmoke;
-        box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.2);
-        position: sticky;
-        top: 10%;
-        margin: 0 auto;
+  
+    let index, offset, progress;
+  </script>
+  
+  <style>
+    section {
+      height: 80vh;
     }
-    
-  /* Scrollytelling CSS */
-  .step {
-    height: 80vh;
-    display: flex;
-    place-items: center;
-    justify-content: center;
-  }
-
-  .step-content {
-    background: whitesmoke;
-    color: #ccc;
-    border-radius: 5px;
-    padding: 0.5rem 1rem;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    transition: background 500ms ease;
-    box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.2);
-    z-index: 10;
-  }
-
-  .step.active .step-content {
-    background: white;
-    color: black;
-  }
-	
-</style>
+  </style>
+  
+  <Scroller top="{0.2}" bottom="{0.8}" bind:index bind:offset bind:progress>
+    <div slot="background">
+      <p>
+        This is the background content. It will stay fixed in place while the
+        foreground scrolls over the top.
+      </p>
+  
+      <p>Section {index + 1} is currently active.</p>
+    </div>
+  
+    <div slot="foreground">
+      <section>This is the first section.</section>
+      <section>This is the second section.</section>
+      <section>This is the third section.</section>
+    </div>
+  </Scroller>
+  
